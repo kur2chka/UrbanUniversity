b3fd6b0ca379b154cac6d1a34dfbb820451bad00
kur2chka <maxpudovmax@gmail.com>
2024-11-19 15:16:04 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+task_quantity =  12
+time_spent = 1.5
+module_name = 'Python'
+time_per_task = time_spent / task_quantity
+print('Курс: ', module_name + ',', 'всего задач: ', str(task_quantity) + ',', 'затрачено часов: ', str(time_spent) + ',', 'среднее время выполнения: ', str(time_per_task) + '.')
