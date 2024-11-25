cf5cf2c229050a8011a62c8f921b81ac69dcb31b
kur2chka <maxpudovmax@gmail.com>
2024-11-25 19:37:28 +0300

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,11 +1,9 @@
 immutable_var = 'netlimiter', 3074, True
-print(immutable_var)
+print('Immutable tuple:', immutable_var)
 #immutable_var[0] = 'legit'
 #команда выше выдаст ошибку, ибо кортеж относится к неизменяемым типам данных
 mutable_list = ['netlimiter', 3074, True]
 mutable_list[0] = 'legit'
 mutable_list[1] = 7500
 mutable_list[2] = False
-print(mutable_list)
-
-
+print('Mutable list: ', mutable_list)
